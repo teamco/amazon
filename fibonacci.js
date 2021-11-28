fbfb053aeed7c12be84003add5699963599879f8
Vladimir Tkach <teamco@gmail.com>
2021-11-28 13:21:31 +0200

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,28 @@
+class Fibonacci {
+  constructor(limit = 1) {  
+    this.store = [0];
+    this.limit = limit;  
+    this.iterate();  
+    this.get();
+  }
+
+  iterate() {
+    for(let i=0; i<this.limit;i ++) {
+      if (i >= this.limit) {
+        // TODO (teamco): Do nothing.
+        break;
+      }
+
+      const _left = this.store[i-2];
+      if (typeof _left === 'undefined') {
+        this.store.push(1);
+      } else {          
+        this.store.push(this.store[i] + this.store[i-1]);
+      }       
+    }   
+  }
+
+  get() {
+    return this.store[this.limit]
+  }
+}
